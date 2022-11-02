8d036cbbec5fb96648ff9d57ba607b91bba0f9f5
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-02 17:03:22 +0530

    setup react router

---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand " to="/">CRUD</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" exact to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" exact to="/About">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" exact to="/Contact">Contact</NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
