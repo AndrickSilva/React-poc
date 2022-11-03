d8ed417dab2fecc62feff34bd17560783e00b748
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-03 15:13:57 +0530

    get data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+    const [users , setUsers] = useState([]);
+
+    useEffect(()=>{
+        getUsers()
+    })
+
+    const getUsers = async () =>{
+        const data = await axios.get("http://localhost:3001/users")
+        console.log(data);
+    }
+
     return (
         <div>
             Im home
