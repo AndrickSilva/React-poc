3b626e80e68edf6494914e5a241b7a8cc258c6c1
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-03 11:10:06 +0530

    added error page img

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import errorImg from '../images/ErrorImg.svg'
 
 const NotFound = () => {
     return (
-        <><h2>404 NOT FOUND</h2></>
+        <div style={{ minHeight: "39.3em", background: "#C6E7FF"}}>
+            <h1>404 NOT FOUND</h1>
+            <img src={errorImg} alt="error" className='img-fluid' style={{ height: "500px", pointerEvents: "none" }} />
+        </div>
     )
 }
 
