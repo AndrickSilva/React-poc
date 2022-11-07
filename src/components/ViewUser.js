4ba201914326bb3590366a25a19f68d48c80bfbe
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-07 10:01:57 +0530

    finished view user

--- src/components/ViewUser.js
+++ src/components/ViewUser.js
@@ -12,7 +12,7 @@ const ViewUser = () => {
         phone: ""
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         loadUser()
     });
 
@@ -21,13 +21,14 @@ const ViewUser = () => {
         setUser(result.data);
     }
     return (
-<div className='container-fluid bg-light pt-4 d-flex justify-content-center align-items-start' style={{minHeight : '110vh'}}>
-            <div className="d-flex gap-3 card justify-content-center align-items-center p-4 rounded-0" style={{maxWidth: 'fit-content'}}>
-                <img src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250" alt='profile' className='rounded-pill' height="100px" width="100px"/>
-                <div className="d-flex flex-column">
-                    <h2 className='fw-bold'>{user.name}</h2>
+        <div className='container-fluid bg-light pt-4 d-flex justify-content-center align-items-start' style={{ minHeight: '110vh' }}>
+            <div className="d-flex gap-3 card-dark justify-content-center align-items-center p-4 rounded-0" style={{ maxWidth: 'fit-content' }}>
+                <img src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250" alt='profile' className='rounded-pill' height="100px" width="100px" />
+                <div className="d-flex flex-column m-3">
+                    <h2 className='fw-bold '>{user.name}</h2>
                     <span className='text-secondary'> {user.username} </span>
                     <p className='text-primary mt-1'>{user.email}</p>
+                    <p className='fw-italic'>{user.phone}</p>
                 </div>
             </div>
         </div>
