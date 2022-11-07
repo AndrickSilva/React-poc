d45d28766c69b598550aee742f3d11351cbfce92
AndrickSilva <silva.andrick2001@gmail.com>
2022-11-07 15:13:43 +0530

    added gradient

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>Im About</div>
+        <div className='container-fluid bg-light' style={{minHeight: "100vh"}}>
+            <p className='container text-justify fw-bold fs-2'><span className="grad fs-1">CRUD</span> refers to the four basic operations a software application should be able to perform – Create, Read, Update, and Delete. In such apps, users must be able to create data, have access to the data in the UI by reading the data, update or edit the data, and delete the data.</p>
+        </div>
     )
 }
 
